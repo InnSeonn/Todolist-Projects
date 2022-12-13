f05c78e4a853d35bdf37e78fa5483caee4260d38
innseonn <innseonn@gmail.com>
2022-12-13 22:50:54 +0900

    Feat: 제목 설정 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useRef, useState } from 'react';
 
 const HeaderLayout = styled.header`
 	max-width: var(--max-width);
@@ -22,11 +23,41 @@ const HeaderInput = styled.input`
 	}
 `;
 
+function getTitle(): string {
+	if(window.localStorage.getItem('todoTitle')) {
+		return String(window.localStorage.getItem('todoTitle'));
+	} else {
+		return '';
+	}
+}
+
 export default function Header() {
+	const textRef = useRef<HTMLInputElement>(null);
+	const [title, setTitle] = useState<string>(getTitle());
+
+	function editTitle() {
+		if(textRef.current !== null) {
+			setTitle(textRef.current.value);
+		}
+	}
+
+	function updateTitle(e: React.FormEvent | React.FocusEvent) {
+		e.preventDefault();
+		if(textRef.current !== null) {
+			window.localStorage.setItem('todoTitle', textRef.current.value);
+		}
+	}
+
+	function checkKeyDown(e: React.KeyboardEvent) {
+		if(e.key === 'Enter' && e.currentTarget instanceof HTMLInputElement) {
+			e.currentTarget.blur();
+		}
+	}
+
 	return (
 		<HeaderLayout className='header'>
-			<HeaderForm action='#'>
-				<HeaderInput type='text' placeholder='2022년 12월에 해야 할 일들' maxLength={15}/>
+			<HeaderForm action='#' onSubmit={updateTitle}>
+				<HeaderInput type='text' placeholder='2022년 12월에 해야 할 일들' maxLength={15} ref={textRef} onChange={editTitle} onBlur={updateTitle} value={title} onKeyDown={checkKeyDown}/>
 			</HeaderForm>
 		</HeaderLayout>
 	);
